ecb3d9938f4d2dcfc62e24a084d59632e4dd3549
Daniel Rines <drines@gmail.com>
2019-05-23 14:15:30 -0700

    docs: added comments to string_repeat.py

--- python/string_repeat.py
+++ python/string_repeat.py
@@ -6,7 +6,9 @@
 # REVISED DATE:     2019.05.23
 # PURPOSE:  String manipulation without causing Memory Error.
 #
-# NOTES:    hackerrank: 
+# NOTES:    Large repeat values will swamp memory if making
+#           a replicate string; thus, need to be clever and 
+#           perform calculation without making a long string.
 #
 #   Example call:
 #      python string_repeat.py
@@ -15,9 +17,13 @@
 s = 'aba'
 n = 1000000000000
 
+# counter to track 'a' occurrances
 a_count = 0
 l = len(s)
 
+# multiply number of 'a's by final string length
+# don't foget the extra values if partial replicate
+# is within the length
 a_count = s.count('a') * (n // l)
 a_count += s[:n % l].count('a')
 
